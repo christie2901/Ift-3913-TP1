72429979192aeca2c976f03d9a9d6aac15059b85
Tiffany224 <133053243+Tiffany224@users.noreply.github.com>
2023-09-27 15:29:35 -0400

    Create tls.java

---
+++ src/tls.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+import java.util.Scanner;
+
+
+
+public class tls {
+
+    public static void tls(String source) throws FileNotFoundException {
+        File folder = new File(source);
+
+        //il faut ignorer les fichiers qui ne sont pas une classe java
+        for (File file : Objects.requireNonNull(folder.listFiles())){
+
+            String filename = file.getName();
+            String chemin = source + "/" + filename;
+            String nomClasse = filename.substring(0,filename.indexOf(".java"));
+
+            //Besoin de scanner pour avoir le package
+            String paquet = null;
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.isBlank()){continue;}
+                if (line.replace(" ","").length()>=2 && line.replace(" ","").substring(0,2).compareTo("/*")==0) {
+                    // ignore les blocs de commentaires
+                    line = scanner.nextLine().replace(" ","");
+                    while (scanner.hasNextLine() && ( line.substring(0,1).compareTo("*") == 0 || line.substring(line.length()-2).compareTo("*/") == 0 ) ) {
+                        line = scanner.nextLine().replace(" ","");
+                    }
+                }
+                if (line.replace(" ","").length()>=2 && !(line.replace(" ","").substring(0,2).compareTo("//")==0)){
+                    if (line.contains("package")){
+                        paquet = line.replace("package","").replace(" ","").replace(";","");
+                        break;
+                    } else if (line.contains("public class")){break;}
+                }
+
+            }
+
+            int nblignes = tloc.lignes(chemin);
+            int nbAssert = tassert.tassert(chemin);
+            double tcmp;
+            if (nbAssert==0) {
+                tcmp = 0.0;
+            } else {
+                tcmp = (double) nblignes / nbAssert;
+            }
+
+            String result = chemin + ", " + paquet + ", " + nomClasse + ", " + nblignes + ", " + nbAssert + ", " + tcmp;
+            //Sortir sur la ligne de commande
+            System.out.println(result);
+
+        }
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        tls("C:/Users/tiffa/Desktop/IFT2255/Robotix_FINAL/Implementation/Robot");
+    }
+
+}
