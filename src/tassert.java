f34b6289353459235033a2db1ebb24342133185e
Tiffany224 <133053243+Tiffany224@users.noreply.github.com>
2023-09-27 18:11:24 -0400

    Update tassert.java

--- src/tassert.java
+++ src/tassert.java
@@ -15,15 +15,15 @@ public class tassert{
             String line = scanner.nextLine();
 
             if (!line.isBlank()){
-                if (line.replace(" ","").length()>=2 && line.replace(" ","").substring(0,2).compareTo("/*")==0) {
+                if (line.contains("/*")) {
                     // ignore les blocs de commentaires
-                    line = scanner.nextLine().replace(" ","");
-                    while (scanner.hasNextLine() && ( line.substring(0,1).compareTo("*") == 0 || line.substring(line.length()-2).compareTo("*/") == 0 ) ) {
-                        line = scanner.nextLine().replace(" ","");
+                    line = scanner.nextLine();
+                    while (scanner.hasNextLine() && ( line.contains("*") || line.contains("*/") ) ) {
+                        line = scanner.nextLine();
                     }
                 }
 
-                if (line.replace(" ","").length()>=2 && !(line.replace(" ","").substring(0,2).compareTo("//")==0)){
+                if (!(line.contains("//"))){
                     if (line.contains("assert") || line.contains("fail")){
                         nbAssert++;
                     }
