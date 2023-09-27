96440f2cc856ddd1a25a4733d174d5435250c655
Tiffany224 <133053243+Tiffany224@users.noreply.github.com>
2023-09-27 18:05:46 -0400

    Update tropcomp.java

--- src/tropcomp.java
+++ src/tropcomp.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class tropcomp {
@@ -12,76 +13,128 @@ public class tropcomp {
         //mon_projet/src/test/java : contient les codes sources des tests
         //mon_projet/src/test/resources : les ressources qui sont utilisées dans les tests
 
-        File folder = new File(source);
+         //dans mon_projet/src/test/java il peut y avoir des dossiers contenant des classes test, des classes test et d'autres fichiers qui ne sont pas des .java
+        String sourceTest = source + "/src/test/java";
+
+
+        File folder = new File(sourceTest);
+
+        ArrayList<File> allFiles = getFiles(folder);
 
         //trouver les fichiers ayant le tloc dans le <seuil>% de tous les fichiers
         //trouver les fichiers ayant le tcomp dans le <seuil>% de tous les fichiers
         //si un fichier est présent dans les deux listes, alors il est suspect
-        String[][] TLOC = new String[Objects.requireNonNull(folder.listFiles()).length][2];
-        String [][] TCOMP = new String[Objects.requireNonNull(folder.listFiles()).length][2];
+        String[][] TLOC = new String[allFiles.size()][2];
+        String [][] TCOMP = new String[allFiles.size()][2];
 
-        int nbSuspects = (int) Math.ceil(Objects.requireNonNull(folder.listFiles()).length*seuil/100);
+        int nbSuspects = (int) Math.ceil(allFiles.size()*seuil/100);
         //si on a 100 fichiers alors le seuil 1% est celui le plus grand
         // si seuil 5%, les 5 plus grands
 
         //collecte de données
         int compte = 0;
-        for (File file : Objects.requireNonNull(folder.listFiles())){
+        for (File file : allFiles){
             int nbLignes = tloc.lignes(file.getPath());
             TLOC[compte][0] = file.getName();
             TLOC[compte][1] = String.valueOf(nbLignes);
 
             int nbAssert = tassert.tassert(file.getPath());
             TCOMP[compte][0] = file.getName();
-            TCOMP[compte][1] = String.valueOf(nbLignes/nbAssert);
+            if (nbAssert==0){
+                TCOMP[compte][1] = String.valueOf(0);
+            } else {
+                TCOMP[compte][1] = String.valueOf(nbLignes/nbAssert);
+            }
 
             compte++;
         }
 
-        TLOC = sort(TLOC);
-        TCOMP = sort(TCOMP);
+        TLOC = sorted(TLOC);
+        TCOMP = sorted(TCOMP);
 
+        String[][] tlocSeuil = new String[nbSuspects][2];
+        String[][] tcompSeuil = new String[nbSuspects][2];
+
+        //Déterminer les fichiers dans le top <seuil>%
+        for (int i=0; i<nbSuspects ; i++){
+            tlocSeuil[i] = TLOC[i];
+            tcompSeuil[i] = TCOMP[i];
+        }
+
+        String[][] suspects = new String[nbSuspects][2];
+        int a = 0;
+        for (int i=0; i<nbSuspects ; i++){
+            for (int j=0; j<nbSuspects ; j++){
+                //tlocSeuil et tcompSeuil n'ont aucune classe en commun, bizarre ...
+                if (tlocSeuil[i][0].equals(tcompSeuil[j][0])){
+                    suspects[a] = tlocSeuil[i];
+                    a++;
+                    break;
+                }
+            }
+            if (a==suspects.length){
+                break;
+            }
+        }
+
+        for (String[] s : suspects){
+            System.out.println(s[0]);
+        }
 
     }
 
-    public static String[][] sort(String[][] list){
-        String[][] result = new String[list.length][list[0].length];
-        String filename = list[0][0];
-        String comp = list[0][1];
+    public static ArrayList<File> getFiles(File folder){
 
-        int compte = 0;
+        int nbFiles = Objects.requireNonNull(folder.listFiles()).length;
+        ArrayList<File> allFiles = new ArrayList<>();
 
-        while (compte < list.length){
-
-            for (int i=0; i<list.length; i++){
-                boolean present = false;
-                filename = list[i][0];
-                for (String[] r : result){
-                    if (r[0].equals(filename)){
-                        present = true;
-                        break;
+        if (nbFiles>0){
+            //dossier ou classe test?
+            for (File file : Objects.requireNonNull(folder.listFiles())){
+                if (file.listFiles() == null){
+                    //fichier
+                    if (file.getName().contains(".java")){
+                        allFiles.add(file);
                     }
-                }
-                if (!present && Integer.parseInt(list[i][1]) > Integer.parseInt(comp)){
-                    comp = list[i][1];
+                } else {
+                    //dossier
+                    allFiles.addAll(getFiles(file));
                 }
             }
+        }
+        return allFiles;
+    }
 
-            result[compte][0] = filename;
-            result[compte][1] = comp;
-
-            compte++;
-
+    public static String[][] sorted(String[][] list){
+        String[][] result = new String[list.length][2];
+        //on compare les valeurs element[i][1] et on veut sauvegarder les noms des classes element[i][0]
+        ArrayList<String[]> pool = new ArrayList<>();
+        for (String[] l : list){
+            pool.add(new String[] {l[0],l[1]});
         }
 
+        int compte = 0;
+        while(!pool.isEmpty()){
+            String[] element = null;
+            String filename = null;
+            String comp = "0";
+            for (String[] strings : pool) {
+                if (Integer.parseInt(strings[1]) >= Integer.parseInt(comp)) {
+                    element = strings;
+                    filename = strings[0];
+                    comp = strings[1];
+                }
+            }
+            result[compte][0] = filename;
+            result[compte][1] = comp;
+            compte++;
+            pool.remove(element);
+        }
         return result;
     }
 
-
     public static void main(String[] args) throws FileNotFoundException {
-        //tropcomp("C:/Users/tiffa/Desktop/IFT2255/Robotix_FINAL/Implementation/Robot",1.0);
-        //tropcomp("C:/Users/tiffa/Desktop/IFT2255/Robotix_FINAL/Implementation/Robot",5.0);
-        //tropcomp("C:/Users/tiffa/Desktop/IFT2255/Robotix_FINAL/Implementation/Robot",10.0);
+        tropcomp("C:\\Users\\tiffa\\Desktop\\IFT3913\\jfreechart-master\\jfreechart-master",1);
 
     }
 }
