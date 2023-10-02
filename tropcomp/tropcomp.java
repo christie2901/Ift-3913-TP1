104eb9fed38326e27a08d26fa1ae2b2bb07f4b26
Tiffany224 <133053243+Tiffany224@users.noreply.github.com>
2023-10-02 15:57:48 -0400

    Update tropcomp.java

--- tropcomp/tropcomp.java
+++ tropcomp/tropcomp.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class tropcomp {
-
-    //seuil int ou double?
     public static void tropcomp(String source, double seuil) throws FileNotFoundException {
 
         //D'abord il faut aller au path des classes tests, configuration Maven
@@ -35,11 +36,11 @@ public class tropcomp {
         int compte = 0;
         for (File file : allFiles){
             int nbLignes = tloc.lignes(file.getPath());
-            TLOC[compte][0] = file.getName();
+            TLOC[compte][0] = file.getPath();
             TLOC[compte][1] = String.valueOf(nbLignes);
 
             int nbAssert = tassert.tassert(file.getPath());
-            TCOMP[compte][0] = file.getName();
+            TCOMP[compte][0] = file.getPath();
             if (nbAssert==0){
                 TCOMP[compte][1] = String.valueOf(0);
             } else {
@@ -61,13 +62,12 @@ public class tropcomp {
             tcompSeuil[i] = TCOMP[i];
         }
 
-        String[][] suspects = new String[nbSuspects][2];
+        String[] suspects = new String[nbSuspects];
         int a = 0;
         for (int i=0; i<nbSuspects ; i++){
             for (int j=0; j<nbSuspects ; j++){
-                //tlocSeuil et tcompSeuil n'ont aucune classe en commun, bizarre ...
                 if (tlocSeuil[i][0].equals(tcompSeuil[j][0])){
-                    suspects[a] = tlocSeuil[i];
+                    suspects[a] = tlocSeuil[i][0];
                     a++;
                     break;
                 }
@@ -77,8 +77,19 @@ public class tropcomp {
             }
         }
 
-        for (String[] s : suspects){
-            System.out.println(s[0]);
+        //sortir dans le format tls
+        ArrayList<String> formatTLS = tls(suspects);
+        //produire un fichier csv
+        if (formatTLS.isEmpty()){
+            System.out.println("Il n'y a pas de classes tests suspectes.");
+        } else {
+            FileWriter fileWriter = new FileWriter("resultats.csv");
+            for (String r : formatTLS){
+                fileWriter.append(r);
+                fileWriter.append("\n");
+            }
+            fileWriter.close();
+
         }
 
     }
@@ -133,8 +144,130 @@ public class tropcomp {
         return result;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        tropcomp("C:\\Users\\tiffa\\Desktop\\IFT3913\\jfreechart-master\\jfreechart-master",1);
+    
+    public static int tloc(String source) throws FileNotFoundException { //prend en paramètre un fichier source d'une classe de test java
+        File classTest = new File(source);
 
+        Scanner scanner = new Scanner(classTest);
+
+        int nbLignes = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if (!(line.compareTo("")==0)){
+                // ignore les lignes vides
+
+                if (line.contains("/*")) {
+                    // ignore les blocs de commentaires
+                    line = scanner.nextLine();
+                    while (scanner.hasNextLine() && ( !(line.contains("*/")))) {
+                        line = scanner.nextLine();
+                    }
+                    continue;
+                }
+
+                if (!(line.contains("//"))){
+                    // ignore les lignes de commentaires et incrémente sinon
+                    nbLignes++;
+
+                } else if (line.replace(" ","").length()==1){
+                    //incrémente s'il y a un caractère }
+                    nbLignes++;
+                }
+            }
+        }
+
+        return nbLignes;
+    }
+
+    public static int tassert(String source) throws FileNotFoundException {
+        File classTest = new File(source);
+
+        Scanner scanner = new Scanner(classTest);
+
+        int nbAssert = 0;
+        while (scanner.hasNextLine()){
+            //si la ligne n'est pas un commentaire et contient "assert" alors nbAssert++
+            String line = scanner.nextLine();
+
+            if (!(line.compareTo("")==0)){
+                if (line.contains("/*")) {
+                    // ignore les blocs de commentaires
+                    line = scanner.nextLine();
+                    while (scanner.hasNextLine() && ( !(line.contains("*/")))) {
+                        line = scanner.nextLine();
+                    }
+                    continue;
+                }
+
+                if (!(line.contains("//"))){
+                    if (line.contains("assert") || line.contains("fail")){
+                        nbAssert++;
+                    }
+                }
+            }
+        }
+
+        return nbAssert;
+    }
+
+    public static ArrayList<String> tls(String[] fichiers) throws FileNotFoundException {
+        ArrayList<String> resultTLS = new ArrayList<>();
+
+
+        for (String fichier : fichiers){
+            if (fichier==null){
+                break;
+            }
+            File file = new File(fichier);
+            String filename = file.getName();
+            String chemin = file.getPath();
+            //S'il y a un fichier pas .java qu'est-ce qu'on fait
+            if (!filename.contains(".java")){
+                continue;
+            }
+            String nomClasse = filename.substring(0,filename.indexOf(".java"));
+
+            //Besoin de scanner pour avoir le package
+            String paquet = null;
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.compareTo("")==0){continue;}
+                if (line.contains("/*")) {
+                    // ignore les blocs de commentaires
+                    line = scanner.nextLine();
+                    while (scanner.hasNextLine() && ( line.contains("*") || line.contains("*/") ) ) {
+                        line = scanner.nextLine();
+                    }
+                }
+                if (!(line.contains("//"))){
+                    if (line.contains("package")){
+                        paquet = line.replace("package","").replace(" ","").replace(";","");
+                        break;
+                    } else if (line.contains("public class")){break;}
+                }
+
+            }
+
+            int nblignes = tloc(chemin);
+            int nbAssert = tassert(chemin);
+            double tcmp;
+            if (nbAssert==0) {
+                tcmp = 0.0;
+            } else {
+                tcmp = (double) nblignes / nbAssert;
+            }
+
+            String result = chemin + ", " + paquet + ", " + nomClasse + ", " + nblignes + ", " + nbAssert + ", " + tcmp;
+            resultTLS.add(result);
+
+        }
+        return resultTLS;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        tropcomp(args[0],args[1]);
+        //"C:\\Users\\tiffa\\Desktop\\IFT3913\\jfreechart-master\\jfreechart-master" 1
     }
 }
