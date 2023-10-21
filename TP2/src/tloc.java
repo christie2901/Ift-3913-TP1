80f76ce6da8ead2953d888d1ea8fbdfe0ad051a8
christie2901 <103332951+christie2901@users.noreply.github.com>
2023-10-21 18:58:13 -0400

    add tloc

---
+++ TP2/src/tloc.java
@@ -0,0 +1,49 @@
+
+//code provenant de notre TP1
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class tloc{
+    //retourne le nombre de lignes de code non-vides qui ne sont pas des commentaires
+    public static int tloc(String source) throws FileNotFoundException {
+        //prend en paramètre un fichier source d'une classe de test java
+        File classTest = new File(source);
+
+        Scanner scanner = new Scanner(classTest);
+
+        int nbLignes = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if (!(line.compareTo("")==0)){
+                // ignore les lignes vides
+
+                if (line.contains("/*")) {
+                    // ignore les blocs de commentaires
+                    line = scanner.nextLine();
+                    while (scanner.hasNextLine() && ( !(line.contains("*/")))) {
+                        line = scanner.nextLine();
+                    }
+                    continue;
+                }
+
+                if (!(line.contains("//"))){
+                    // ignore les lignes de commentaires et incrémente sinon
+                    nbLignes++;
+
+                } else if (line.replace(" ","").length()==1){
+                    //incrémente s'il y a un caractère }
+                    nbLignes++;
+                }
+            }
+        }
+
+        return nbLignes;
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(tloc(args[0]));
+        //Robot/Taches.java doit donner 321 "C:/Users/tiffa/Desktop/IFT2255/Robotix_FINAL/Implementation/Robot/Taches.java"
+        //UtilitairesFichier.java doit donner 62
+        //"C:/Users/tiffa/Desktop/IFT3913/TP1_tester.txt" 19
+    }
+}
